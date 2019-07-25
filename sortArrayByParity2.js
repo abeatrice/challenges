d59a93de2865f32911468beda776a00c49d49ec0
Andrew Beatrice <abeatrice@lampsplus.com>
2019-07-25 08:29:44 -0700

    another solution for sort array by parity 2

--- sortArrayByParity2.js
+++ sortArrayByParity2.js
@@ -1,3 +1,7 @@
+var even = function (n) {
+    return n % 2 == 0;
+}
+
 /**
  * Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even.
  * Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
@@ -6,21 +10,24 @@
  * @return {number[]}
  */
 var sortArrayByParityII = function (A) {
-    let even = [];
-    let odd = [];
-    let res = [];
-
-    A.forEach(n => (n % 2 == 0) ? even.push(n) : odd.push(n));
-
     for (i = 0; i < A.length; i++) {
-        if (i % 2 == 0) {
-            res.push(even.pop());
-        } else {
-            res.push(odd.pop());
+        if (even(i) && even(A[i])) {
+            continue;
+        }
+        if (!even(i) && !even(A[i])) {
+            continue;
+        }
+
+        for (j = i + 1; j < A.length; j++) {
+            if ((even(i) && even(A[j])) || (!even(i) && !even(A[j]))) {
+                var temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+                break;
+            }
         }
     }
-
-    return res;
+    return A;
 };
 
 console.log(sortArrayByParityII([4, 2, 5, 7]));
