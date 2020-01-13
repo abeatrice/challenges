bccecf931a2921fc37035d53a67efa7dbf3d244f
Andrew Beatrice <abeatrice@lampsplus.com>
2020-01-13 12:56:45 -0800

    range sum binary search tree

--- rangeSumBST.php
+++ rangeSumBST.php
@@ -8,6 +8,29 @@ class TreeNode {
     function __construct($value) { $this->val = $value; }
 }
 
+class Solution {
+    
+    public function rangeSumBST($root, $L, $R)
+    {
+        return $this->getSum($root, $L, $R, 0);
+    }
+    
+    public function getSum($root, $L, $R, $sum) {
+        
+        if ($root === NULL) return $sum;
+        
+        if ($root->val >= $L && $root->val <= $R) {
+            $sum += $root->val;
+            $sum = $this->getSum($root->right, $L, $R, $sum);
+            return $this->getSum($root->left, $L, $R, $sum);
+        } elseif ($root->val > $R) {
+            return $this->getSum($root->left, $L, $R, $sum);
+        } else {
+            return $this->getSum($root->right, $L, $R, $sum);
+        }
+    }
+}
+    
 $root = new TreeNode(10);
 $root->left = new TreeNode(5);
 $root->left->left = new TreeNode(3);
@@ -15,18 +38,5 @@ $root->left->right = new TreeNode(7);
 $root->right = new TreeNode(15);
 $root->right->right = new TreeNode(18);
 
-function rangeSumBST($root, $L, $R)
-{
-    $currentNode = $root;
-    var_dump($currentNode);
-    while($currentNode->val !== $L) {
-        if($currentNode->val > $L) {
-            $currentNode = $currentNode->left;
-        } else {
-            $currentNode = $currentNode->right;
-        }
-    }
-    var_dump($currentNode);
-}
-
-var_dump(rangeSumBST($root, 7, 15));
+$solution = (new Solution())->rangeSumBST($root, 7, 15);
+var_dump($solution);
